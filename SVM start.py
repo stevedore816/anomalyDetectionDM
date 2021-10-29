c894d0530adb0904e8ead172132fe89743841a96
Steven Arroyo <arroyo47@students.rowan.edu>
2021-10-28 23:01:23 -0400

    Some type of plan in place to preprocess based on distance correlation

--- SVM start.py
+++ SVM start.py
@@ -9,7 +9,9 @@ import numpy as np
 
 db = pd.read_csv("Data/benign_traffic.csv")
 anomaly = pd.read_csv("Data/gafgyt_attacks/scan.csv")
-
+"""Plan currently is to preprocess the data to make each feature have a mean of 0
+and a std. of 1 across so that we can use correlation as a distance measure to than add to a distance correlation matrix
+which will than result in clustering based on generic correlation for the AD ensemble algorithm"""
 def returnDB(search):
     Array = []
     for i in db.keys():
@@ -28,29 +30,31 @@ Hp = returnDB("Hp")
 HH = returnDB("HH")
 H = returnDB('H_')
 
-train = MI.iloc[:int(.8*len(MI)),:]
-trainx = MI.iloc[int(.4*len(train)):,:]
-validx = MI.iloc[:int(.4*len(train)),:]
-traintest = MI.iloc[int(.8*len(MI)):,:]
+train = db.iloc[:int(.8*len(MI)),:]
+trainx = db.iloc[int(.4*len(train)):,:]
+validx = db.iloc[:int(.4*len(train)),:]
+traintest = db.iloc[int(.8*len(MI)):,:]
 
 traintf = tf.convert_to_tensor(trainx)
 validtf = tf.convert_to_tensor(validx)
 tttf = tf.convert_to_tensor(traintest)
 print(traintf)
-
+"""
+This is all neural net stuff but preprocessing of the values still needs to happen before I worry about this
 autoencoder1 = Sequential()
-autoencoder1.add(Dense(8, activation='elu',input_shape=(15,)))
-autoencoder1.add(Dense(4,activation='elu'))
-autoencoder1.add(Dense(2,activation='linear'))
-autoencoder1.add(Dense(4,activation='elu')) #This is the compressed info
-autoencoder1.add(Dense(8,activation='elu'))
-autoencoder1.add(Dense(15, activation='linear'))
+autoencoder1.add(Dense(115, activation='elu',input_shape=(115,)))
+autoencoder1.add(Dense(64, activation='elu'))
+autoencoder1.add(Dense(32, activation='elu'))
+autoencoder1.add(Dense(15,activation='linear'))
+autoencoder1.add(Dense(32, activation='elu'))
+autoencoder1.add(Dense(64, activation='linear'))
+
 autoencoder1.compile(loss='mean_squared_error', optimizer='adam')
-trained=autoencoder1.fit(traintf,traintf,epochs=60,validation_data=(validtf,validtf))
+trained=autoencoder1.fit(traintf,traintf,epochs=15,validation_data=(validtf,validtf))
 
 print(autoencoder1.evaluate(tttf,tttf))
 
-test = MI.iloc[78:79, :]
+test = MI.iloc[:1, :]
 testtf = tf.convert_to_tensor(test)
 target_data = autoencoder1.predict(testtf)
 dist = np.linalg.norm(test - target_data, axis = -1)
@@ -62,3 +66,4 @@ target_data = autoencoder1.predict(anon)
 autoencoder1.evaluate(anon,anon)
 dist = np.linalg.norm(anon - target_data, axis = -1)
 print(dist)
+"""
