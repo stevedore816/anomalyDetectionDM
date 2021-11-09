5620a8f51ef0c967e5152ad28f5dbac5751d8182
Steven Arroyo <arroyo47@students.rowan.edu>
2021-11-09 15:58:35 -0500

    After creating the autoencoder algorithm, I then put the autoencoder ensemble into a function I wrote that outputs a completed neural network. All the loss functions are <= 0.02 which is perfect and are able to tell the anomalies apart

--- SVM start.py
+++ SVM start.py
@@ -4,6 +4,7 @@ import keras
 from keras.models import Sequential, Model
 from keras.layers import Dense, Input
 from keras import optimizers
+from scipy.cluster import hierarchy as hc
 import numpy as np
 
 db = pd.read_csv("Data/benign_traffic.csv")
@@ -19,10 +20,12 @@ for column in db.keys():
     # This one line calculates the inverse of the mean and subtracts all the attributes by it so mean = 0, and than / by the standard deviation across making the std = 1
     db[column] = (db[column] + (-1 * db[column].mean())) / (db[column].std())
     print(db[column].mean(), db[column].std())
-    # Create Correlation Matrix
+# Create Correlation Matrix
 dbCorr = db.corr()
 print(dbCorr)
-
+for column in anomaly.keys():
+    # This one line calculates the inverse of the mean and subtracts all the attributes by it so mean = 0, and than / by the standard deviation across making the std = 1
+    anomaly[column] = (anomaly[column] + (-1 * anomaly[column].mean())) / (anomaly[column].std())
 
 
 """Logic for Autoencoder Implementation
@@ -41,7 +44,7 @@ autoencoders = []
 explored = []
 def createAutoEncoderAttribute(row):
 
-    if len(autoencoders) < 8:
+    if len(autoencoders) < 9:
         autoencoder = []
         greater = True
         explored.append(row)
@@ -49,7 +52,6 @@ def createAutoEncoderAttribute(row):
             if len(autoencoder) <= 15:
                 if dbCorr[row][column] >= .75 and column not in explored:
                     autoencoder.append(column)
-                    #explored.append(column)
 
         smallest = 1000
         for encode in autoencoders:
@@ -73,6 +75,22 @@ def createAutoEncoderAttribute(row):
 
 for row in Key:
     createAutoEncoderAttribute(row)
+
+def getAutoEncoder(num,anon):
+    tempDB = pd.DataFrame()
+    for i in range (0, len(autoencoders[num]) - 1):
+        attr = autoencoders[num][i]
+        if anon:
+            tempDB[attr] = anomaly[attr]
+        else:
+            tempDB[attr] = db[attr]
+    return tempDB
+#autoencoder1 = getAutoEncoder(0,anon= False)
+#anomaly1 = getAutoEncoder(0,anon = True)
+#print(autoencoder1)
+
+
+#Old method That I will delete alter doesn't do anything
 def returnDB(search):
     Array = []
     for i in db.keys():
@@ -86,47 +104,59 @@ def returnDB(search):
         DBMI[x] = db[x]
     return DBMI
 
+"""This function will create a neural net with the previously provided information and create a autoencoder with it
+autoencoderDB is the dataframe from pandas that handles the autoencoderDB
+input is the shape that you are inputting into the neural network first
+layers is the neurons in each layer(7 has to be odd) of the neural network
+middle is the compression layer of the neural network or the point you want it at
+disttest is a variable to see if we're testing the neural net based on dist or not
+anomalyDB is the anomalydetection we're going to be using that correlate to those respective attr during testing
 """
-#Naive Approach
-MI = returnDB("MI")
-Hp = returnDB("Hp")
-HH = returnDB("HH")
-H = returnDB('H_')
+matrixout = []
+def createNeuralNet(autoencoderDB,input, layers, middle,disttest,anomalyDB):
+    #autoencoderDB = returnDB('MI')
+    train = autoencoderDB.iloc[:int(.8 * len(autoencoderDB)), :]
+    trainx = train.iloc[int(.4 * len(train)):, :]
+    validx = train.iloc[:int(.4 * len(train)), :]
+    traintest = autoencoderDB.iloc[int(.8 * len(autoencoderDB)):, :]
 
-train = MI.iloc[:int(.8 * len(MI)), :]
-trainx = train.iloc[int(.4 * len(train)):, :]
-validx = train.iloc[:int(.4 * len(train)), :]
-traintest = MI.iloc[int(.8 * len(MI)):, :]
+    traintf = tf.convert_to_tensor(trainx)
+    validtf = tf.convert_to_tensor(validx)
+    tttf = tf.convert_to_tensor(traintest)
+    #print(validtf, traintf)
 
-traintf = tf.convert_to_tensor(trainx)
-validtf = tf.convert_to_tensor(validx)
-tttf = tf.convert_to_tensor(traintest)
-print(traintf)
+    autoencoder = Sequential()
+    autoencoder.add(Dense(layers[0], activation='elu', input_shape=(input,)))
+    for i in range (1, len(layers)):
+        if i == middle:
+            autoencoder.add(Dense(layers[middle], activation='linear'))
+        else:
+            autoencoder.add(Dense(layers[i], activation='elu'))
 
-#This is all neural net stuff but preprocessing of the values still needs to happen before I worry about this
-autoencoder1 = Sequential()
-autoencoder1.add(Dense(10, activation='elu',input_shape=(15,)))
-autoencoder1.add(Dense(8, activation='elu'))
-autoencoder1.add(Dense(4,activation='linear'))
-autoencoder1.add(Dense(8, activation='elu'))
-autoencoder1.add(Dense(10, activation='elu'))
-autoencoder1.add(Dense(15, activation='linear'))
+    autoencoder.compile(loss='mean_squared_error', optimizer='adam')
+    trained = autoencoder.fit(traintf,traintf,epochs=15,validation_data=(validtf,validtf))
 
-autoencoder1.compile(loss='mean_squared_error', optimizer='adam')
-trained=autoencoder1.fit(traintf,traintf,epochs=15,validation_data=(validtf,validtf))
-
-print(autoencoder1.evaluate(tttf,tttf))
-
-test = MI.iloc[:1, :15]
-testtf = tf.convert_to_tensor(test)s
-target_data = autoencoder1.predict(testtf)
-dist = np.linalg.norm(test - target_data, axis = -1)
-print(dist)
-anon = anomaly.iloc[1:2, :15]
-print(anon)
-anon = tf.convert_to_tensor(anon)
-target_data = autoencoder1.predict(anon)
-autoencoder1.evaluate(anon,anon)
-dist = np.linalg.norm(anon - target_data, axis = -1)
-print(dist)
-"""
+    print("Test Eval", autoencoder.evaluate(tttf,tttf))
+    if disttest:
+        test = autoencoderDB.iloc[13:14, :]
+        testtf = tf.convert_to_tensor(test)
+        target_data = autoencoder.predict(testtf)
+        distg = np.linalg.norm(test - target_data, axis = -1)
+        #print(dist)
+        anon = anomalyDB.iloc[3:4,:]
+        #print(anon)
+        anon = tf.convert_to_tensor(anon)
+        target_data = autoencoder.predict(anon)
+        autoencoder.evaluate(anon,anon)
+        dist = np.linalg.norm(anon - target_data, axis = -1)
+        #print(dist)
+        matrixout.append((distg,dist))
+    return autoencoder
+auto1 = createNeuralNet(getAutoEncoder(0,anon=False), 16, [10,8,4,2,4,8,10,16],3,disttest= True, anomalyDB=getAutoEncoder(0,anon=True))
+auto2 = createNeuralNet(getAutoEncoder(1,anon=False), 16, [10,8,4,2,4,8,10,16],3,disttest= True, anomalyDB=getAutoEncoder(1,anon=True))
+auto3 = createNeuralNet(getAutoEncoder(2,anon=False), 9, [8,4,2,4,8,9],2,disttest= True, anomalyDB=getAutoEncoder(2,anon=True))
+auto4 = createNeuralNet(getAutoEncoder(3,anon=False), 6, [4,2,4,6],1,disttest= True, anomalyDB=getAutoEncoder(3,anon=True))
+auto5 = createNeuralNet(getAutoEncoder(4,anon=False), 9, [8,4,2,4,8,9],2,disttest= True, anomalyDB=getAutoEncoder(4,anon=True))
+auto6 = createNeuralNet(getAutoEncoder(5,anon=False), 7, [4,2,4,7],1,disttest= True, anomalyDB=getAutoEncoder(5,anon=True))
+auto7 = createNeuralNet(getAutoEncoder(6,anon=False), 6, [4,2,4,6],1,disttest= True, anomalyDB=getAutoEncoder(6,anon=True))
+print(matrixout)
